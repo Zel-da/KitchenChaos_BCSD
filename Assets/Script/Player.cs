190119b5caf1a06d2a767b741f832df2b1375292
Zel-da <anjongman1029@gmail.com>
2024-05-07 00:13:12 +0900

    ClearCounter

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] 
-    private float moveSpeed = 7f;
-
-    [SerializeField] 
-    private GameInput gameInput;
-
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
+    
     private bool isWalking;
+    private Vector3 lastInteractDir;
+    
     void Start()
     {
 
@@ -18,6 +18,39 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+        
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+    }
+    
+    private void HandleMovement()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         
@@ -70,10 +103,5 @@ public class Player : MonoBehaviour
         
         float rotateSpeed = 30f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-    }
-
-    public bool IsWalking()
-    {
-        return isWalking;
     }
 }

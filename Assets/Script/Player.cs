5649cdf5cf9e2b4b57a1422712c0557638c1930d
Zel-da <anjongman1029@gmail.com>
2024-05-06 21:48:43 +0900

    Animations

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
 
+    private bool isWalking;
     void Start()
     {
 
@@ -38,8 +39,16 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform. position += moveDir * moveSpeed * Time.deltaTime;
 
-        float rotateSpeed = 10f;
+        isWalking = moveDir != Vector3.zero;
+        
+        float rotateSpeed = 30f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
+        
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
